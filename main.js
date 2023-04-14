93db45d5c4b9ecc37f6f9ee4c8e70825cf512cec
2.ruthless <105881276+DaRuthlessPWR@users.noreply.github.com>
2023-04-14 04:28:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const endpoints = {
 
-assetDelivery: id => `https://assetdelivery.roblox.com/v1/asset/?id=${id}`, publish: (title, description, groupId) =>
-
-'https://www.roblox.com/ide/publish/uploadnewanimation' +
-
-'?assetTypeName=Animation' +
-
-*&name=${encodeURIComponent(title)}` +
-
-`&description=${encodeURIComponent(description)}`
-
-'&AllID=1' +
-
-'&ispublic-False' +
-
-'&allowComments-True' +
-
-'&isGamesAsset=False' +
-
-(groupId != null ? `&groupId=${groupId}` : '')
+  assetDelivery: id => `https://assetdelivery.roblox.com/v1/asset/?id=${id}`, 
+  publish: (title, description, groupId) =>
+      'https://www.roblox.com/ide/publish/uploadnewanimation' +
+      '?assetTypeName=Animation' +
+      *&name=${encodeURIComponent(title)}` +
+      `&description=${encodeURIComponent(description)}`
+      '&AllID=1' +
+      '&ispublic-False' +
+      '&allowComments-True' +
+      '&isGamesAsset=False' +
+      (groupId != null ? `&groupId=${groupId}` : '')
 
 };
